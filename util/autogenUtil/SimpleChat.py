272b51a0f08fb893321b584e0b0e6daf574938f4
griftt <13538774847@163.com>
2024-03-26 22:42:42 +0800

    工具调用

--- util/autogenUtil/SimpleChat.py
+++ util/autogenUtil/SimpleChat.py
@@ -1,44 +1,56 @@
+import tempfile
 from typing import Optional, Any, List, Dict, Union, Tuple
 
 import autogen
 from autogen import ChatResult, Agent, ConversableAgent
+from autogen.coding import LocalCommandLineCodeExecutor
 
 from autogenDmo.simpleChat import callBack
 from config_list import config_list_mistral
+import streamlit as st
 
+# Create a temporary directory to store the code files.
+temp_dir = tempfile.TemporaryDirectory()
 
-
+# Create a local command line code executor.
+executor = LocalCommandLineCodeExecutor(
+    timeout=10,  # Timeout for each code execution in seconds.
+    work_dir=temp_dir.name,  # Use the temporary directory to store the code files.
+)
 def callBack(
                     recipient: ConversableAgent,
                     messages: Optional[List[Dict]] = None,
                     sender: Optional[Agent] = None,
                     config: Optional[Any] = None,
                 ) :
-    if messages is not None and messages[-1] is not None:
-        print("Dddd", messages[-1]["content"])
-        st.session_state.messages.append({"role": "AI", "content": messages[-1]["content"]})
+    # if messages is not None and messages[-1] is not None:
+    #     print("111", messages[-1]["content"])
+    #     st.session_state.messages.append({"role": "AI", "content": messages[-1]["content"]})
+    #     st.session_state.msgContainer.chat_message("AI").write(messages[-1]["content"])
 
     return (True, None)
 
 class SimpleChat:
-    def initSimeDoubleChat(self,promt) :
+    def initSimeDoubleChat(self,promt):
         chinese = autogen.AssistantAgent("chinese", llm_config={"config_list": config_list_mistral},
-                                         system_message="you good at math",
-                                         max_consecutive_auto_reply=1)
+                                         system_message="你是一个顶级程序员"
+                                         )
         # american = autogenUtil.AssistantAgent("", llm_config={"config_list": config_list_mistral},
         #                                   system_message="你是一个知识渊博的美国人，你要向大家介绍美国文化",
         #                                   max_consecutive_auto_reply=1)
         # korea = autogenUtil.AssistantAgent("chinese", llm_config={"config_list": config_list_mistral},
         #                                system_message="你是一个知识渊博的韩国人，你要向大家介绍韩国文化",
         #                                max_consecutive_auto_reply=1)
-
+        #简单调用工具
         host = autogen.UserProxyAgent("host",
+                                      max_consecutive_auto_reply=4,
                                       # llm_config={"config_list": config_list_mistral},
-                                      code_execution_config={"work_dir": "coding",
-                                                             "use_docker": False})
+                                      human_input_mode="NEVER",
+                                      code_execution_config={"executor": executor,})
         host.register_reply([Agent, None], reply_func=callBack)
-        host.initiate_chat(chinese, message=promt, max_turns=2)
+        host.initiate_chat(chinese,message="帮我写程序计算从1 加到100 总和是多少", max_turns=4)
+        #host.send(promt,chinese)
 
 
-
-
+caht=SimpleChat()
+caht.initSimeDoubleChat("1+11等于多少")
--- web.py
+++ web.py
@@ -1,40 +1,16 @@
-from typing import Optional
-
 import streamlit as st
-import numpy as np
-import pandas as pd
-import time
+from typing import Optional, Any, List, Dict
+from autogen import Agent, ConversableAgent
 import autogen
-import streamlit as st
-from typing import Optional, Any, List, Dict, Union, Tuple
-from autogen import ChatResult, Agent, ConversableAgent
 
 from config_list import config_list_mistral
-
+from util.autogenUtil.SimpleChat import SimpleChat
 
 # st.sidebar.page_link("web.py", label="Home", icon="🏠")
 # st.sidebar.page_link("pages/llm.py", label="Page 1", icon="1️⃣")
 # st.sidebar.page_link("pages/know.py", label="Page 2", icon="2️⃣")
 # st.sidebar.page_link("http://www.google.com", label="Google", icon="🌎")
-class SimpleChat:
-    def initSimeDoubleChat(self,promt) :
-        chinese = autogen.AssistantAgent("chinese", llm_config={"config_list": config_list_mistral},
-                                         system_message="you good at math",
-                                         max_consecutive_auto_reply=1)
-        # american = autogenUtil.AssistantAgent("", llm_config={"config_list": config_list_mistral},
-        #                                   system_message="你是一个知识渊博的美国人，你要向大家介绍美国文化",
-        #                                   max_consecutive_auto_reply=1)
-        # korea = autogenUtil.AssistantAgent("chinese", llm_config={"config_list": config_list_mistral},
-        #                                system_message="你是一个知识渊博的韩国人，你要向大家介绍韩国文化",
-        #                                max_consecutive_auto_reply=1)
 
-        host = autogen.UserProxyAgent("host",
-                                      human_input_mode="ALWAYS",
-                                      # llm_config={"config_list": config_list_mistral},
-                                      code_execution_config={"work_dir": "coding",
-                                                             "use_docker": False})
-        host.register_reply([Agent, None], reply_func=callBack)
-        host.initiate_chat(chinese, message=promt, max_turns=2)
 
 #左侧的提交表格
 st.title('Griftt 的 :blue[智能管家] :sunglasses:',anchor ='false')
@@ -54,10 +30,32 @@ form.selectbox(
 # Now add a submit button to the form:
 form.form_submit_button("Submit")
 
+class SimpleChat:
+
+    def __init__(self,roleMessage,func_reply):
+        self.assistant =autogen.AssistantAgent("assistant", llm_config={"config_list": config_list_mistral},
+                                         system_message=roleMessage,
+                                         max_consecutive_auto_reply=2)
+        self.userProxy = autogen.UserProxyAgent("host",
+                                      # llm_config={"config_list": config_list_mistral},
+                                      code_execution_config={"work_dir": "coding",
+                                                             "use_docker": False})
+        self.userProxy.register_reply([Agent, None], reply_func=func_reply)
+
+
+    def initSimeDoubleChat(self,promt) :
+
+        # american = autogenUtil.AssistantAgent("", llm_config={"config_list": config_list_mistral},
+        #                                   system_message="你是一个知识渊博的美国人，你要向大家介绍美国文化",
+        #                                   max_consecutive_auto_reply=1)
+        # korea = autogenUtil.AssistantAgent("chinese", llm_config={"config_list": config_list_mistral},
+        #                                system_message="你是一个知识渊博的韩国人，你要向大家介绍韩国文化",
+        #                                max_consecutive_auto_reply=1)
+        self.userProxy.initiate_chat(self.assistant, message=promt, max_turns=4)
 
 
 #main
-tab1, tab2 = st.tabs(["📈 人机互动", "🗃 AI群聊"])
+tab1, tab2 = st.tabs(["📈 私人管家", "🗃 AI团队"])
 tab1.subheader("在这里调试你的一对一人机互动操作")
 #选择需要使用的大模型
 #单聊
@@ -81,8 +79,13 @@ for message in st.session_state.messages:
     # with tab1.chat_message(message["role"]):
     msgContainer.chat_message(message["role"]).markdown(message["content"])
 st.write(st.session_state.messages)
-#初始化聊天
-simpleChat=SimpleChat()
+
+
+
+
+
+
+##获取聊天回复信息
 def callBack(recipient: ConversableAgent,
                     messages: Optional[List[Dict]] = None,
                     sender: Optional[Agent] = None,
@@ -92,12 +95,12 @@ def callBack(recipient: ConversableAgent,
     if messages is not None and messages[-1] is not None:
         print("Dddd", messages)
         msgContainer.chat_message("AI").write(messages[-1]["content"])
-        #     st.session_state.messages.append({"role": "AI", "content": message["content"]})
+        #st.session_state.messages.append({"role": "AI", "content": message["content"]})
         st.session_state.messages.append({"role": messages[-1]["role"], "content": messages[-1]["content"]})
 
     return (True, None)
-
-
+#初始化聊天
+simpleChat=SimpleChat("你是一个全能的llm大模型可以回答我的任何问题",callBack)
 
 
 if prompt := tab1.chat_input("输入你的问题"):
@@ -122,9 +125,6 @@ if prompt := tab1.chat_input("输入你的问题"):
 
 
 tab2.subheader("AI团队帮你完成任务")
-
-
-
 # Delete all the items in Session state
 # for key in st.session_state.keys():
 #     del st.session_state[key]
