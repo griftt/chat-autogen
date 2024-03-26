427ae3014785e343c782940ab632811e6b65ae51
griftt <13538774847@163.com>
2024-03-26 22:56:15 +0800

    工具调用

--- util/autogenUtil/SimpleChat.py
+++ util/autogenUtil/SimpleChat.py
@@ -8,7 +8,22 @@ from autogen.coding import LocalCommandLineCodeExecutor
 from autogenDmo.simpleChat import callBack
 from config_list import config_list_mistral
 import streamlit as st
+from typing import Annotated, Literal
 
+
+##计算工具
+Operator = Literal["+", "-", "*", "/"]
+def calculator(a: int, b: int, operator: Annotated[Operator, "operator"]) -> int:
+    if operator == "+":
+        return a + b
+    elif operator == "-":
+        return a - b
+    elif operator == "*":
+        return a * b
+    elif operator == "/":
+        return int(a / b)
+    else:
+        raise ValueError("Invalid operator")
 # Create a temporary directory to store the code files.
 temp_dir = tempfile.TemporaryDirectory()
 
@@ -35,6 +50,11 @@ class SimpleChat:
         chinese = autogen.AssistantAgent("chinese", llm_config={"config_list": config_list_mistral},
                                          system_message="你是一个顶级程序员"
                                          )
+        # Register the tool signature with the assistant agent.
+        #注册让助手可以建议用户代理调用
+        chinese.register_for_llm(name="calculator", description="A simple calculator")(calculator)
+
+
         # american = autogenUtil.AssistantAgent("", llm_config={"config_list": config_list_mistral},
         #                                   system_message="你是一个知识渊博的美国人，你要向大家介绍美国文化",
         #                                   max_consecutive_auto_reply=1)
@@ -48,7 +68,24 @@ class SimpleChat:
                                       human_input_mode="NEVER",
                                       code_execution_config={"executor": executor,})
         host.register_reply([Agent, None], reply_func=callBack)
+        # Register the tool function with the user proxy agent.
+        #用户代理可以调用的方法
+        host.register_for_execution(name="calculator")(calculator)
+
+        from autogen import register_function
+
+        # Register the calculator function to the two agents.
+        #也可以通过这个方法向两个代理注册工具
+        # register_function(
+        #     calculator,
+        #     caller=assistant,  # The assistant agent can suggest calls to the calculator.
+        #     executor=user_proxy,  # The user proxy agent can execute the calculator calls.
+        #     name="calculator",  # By default, the function name is used as the tool name.
+        #     description="A simple calculator",  # A description of the tool.
+        # )
+
         host.initiate_chat(chinese,message="帮我写程序计算从1 加到100 总和是多少", max_turns=4)
+
         #host.send(promt,chinese)
 
 
